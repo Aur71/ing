f8472f6b42636a42bac68608a355cdc0e6fd9ff7
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-12-12 15:35:42 +0200

    Hero frontend done

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -1,76 +1,17 @@
 import styles from '../../styles/Hero.module.scss'
 import { useGlobalContext } from '../../context/context'
 
-import Link from 'next/link'
-import Image from 'next/image'
-
-import javascript from '../../public/temp/javascript.png'
-import coding from '../../public/temp/coding.jpg'
-import computer from '../../public/temp/computer.jpg'
-import user from '../../public/temp/user.jpg'
+import Hero1 from './Hero1'
+import Hero2 from './Hero2'
 
 const Hero = () => {
   const { search } = useGlobalContext()
 
   if (search !== '') {
-    return (
-      <div className={styles.hero}>
-        <div className={styles.center}>search</div>
-      </div>
-    )
+    return <Hero2 />
   }
 
-  return (
-    <div className={styles.hero}>
-      <div className={styles.center}>
-        <Link href='/' className={styles.postShowcase}>
-          <div className={styles.imgContainer}>
-            <Image priority={true} src={coding} alt='temp' />
-          </div>
-
-          <div className={styles.textContainer}>
-            <h2>How to become a more eficient programmer</h2>
-
-            <div className={styles.info}>
-              <Image src={user} alt='user image' />
-
-              <h4>
-                Jhon Doe <span>·</span> December 29, 2020
-              </h4>
-            </div>
-          </div>
-        </Link>
-
-        <Link href='/' className={styles.postShowcase}>
-          <div className={styles.imgContainer}>
-            <Image src={javascript} alt='temp' />
-          </div>
-
-          <div className={styles.textContainer}>
-            <h2>7 Javascript Tips</h2>
-
-            <h4>
-              Jhon Doe <span>·</span> December 29, 2020
-            </h4>
-          </div>
-        </Link>
-
-        <Link href='/' className={styles.postShowcase}>
-          <div className={styles.imgContainer}>
-            <Image src={computer} alt='temp' />
-          </div>
-
-          <div className={styles.textContainer}>
-            <h2>What computer do you need as a software engeneier?</h2>
-
-            <h4>
-              Jhon Doe <span>·</span> December 29, 2020
-            </h4>
-          </div>
-        </Link>
-      </div>
-    </div>
-  )
+  return <Hero1 />
 }
 
 export default Hero
