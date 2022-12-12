c50b4adb52a5d7fa4837308988ba4aff0ce2b608
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-12-12 17:20:10 +0200

    Cards added

---
+++ components/home/Post.jsx
@@ -0,0 +1,33 @@
+import styles from '../../styles/Post.module.scss'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import coding from '../../public/temp/coding.jpg'
+
+const Post = () => {
+  return (
+    <Link href='/' className={styles.post}>
+      <Image src={coding} alt='image' />
+
+      <div className={styles.textContainer}>
+        <h5>December 29, 2020</h5>
+
+        <h4>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus
+          provident numquam recusandae rerum eius, non beatae optio possimus
+          nostrum illum?
+        </h4>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          ipsa reiciendis adipisci minima repellat molestias aperiam harum
+          atque, voluptas dolorem consequuntur iusto tempora, eos sint veniam
+          similique ratione quisquam ab.
+        </p>
+      </div>
+    </Link>
+  )
+}
+
+export default Post
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import styles from '../styles/Home.module.scss'
 
 import Hero from '../components/home/Hero'
+import Post from '../components/home/Post'
 
 export default function Home() {
   return (
     <section className={styles.home}>
       <Hero />
+
+      <div className={styles.postContainer}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </section>
   )
 }
