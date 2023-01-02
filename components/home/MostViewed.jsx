aa2314be8a21c0c99cfcd4087686629206b09d9b
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-01-02 11:04:59 +0200

    Most viewed section added

--- components/home/MostViewed.jsx
+++ components/home/MostViewed.jsx
@@ -1,7 +1,81 @@
-import React from 'react';
+import styles from '../../styles/home/MostViewed.module.scss';
+// OTHER
+import Image from 'next/image';
+import Link from 'next/link';
+
+// MEDIA
+import img from '../../public/temp/computer.jpg';
 
 const MostViewed = () => {
-  return <div>MostViewed</div>;
+  return (
+    <div className={styles.mostviewed}>
+      <article className={styles.card}>
+        <div className={styles.imgContainer}>
+          <Image src={img} alt='thumbnail' />
+        </div>
+
+        <Link href='#'>
+          <div className={styles.textContainer}>
+            <h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
+              eligendi.
+            </h3>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              est officiis nulla, corporis sit veniam, iusto repudiandae impedit
+              accusantium totam obcaecati voluptate, accusamus consequatur
+              magnam adipisci minima ducimus ipsam blanditiis.
+            </p>
+          </div>
+        </Link>
+      </article>
+
+      <article className={styles.card}>
+        <div className={styles.imgContainer}>
+          <Image src={img} alt='thumbnail' />
+        </div>
+
+        <Link href='#'>
+          <div className={styles.textContainer}>
+            <h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
+              eligendi.
+            </h3>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              est officiis nulla, corporis sit veniam, iusto repudiandae impedit
+              accusantium totam obcaecati voluptate, accusamus consequatur
+              magnam adipisci minima ducimus ipsam blanditiis.
+            </p>
+          </div>
+        </Link>
+      </article>
+
+      <article className={styles.card}>
+        <div className={styles.imgContainer}>
+          <Image src={img} alt='thumbnail' />
+        </div>
+
+        <Link href='#'>
+          <div className={styles.textContainer}>
+            <h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
+              eligendi.
+            </h3>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
+              est officiis nulla, corporis sit veniam, iusto repudiandae impedit
+              accusantium totam obcaecati voluptate, accusamus consequatur
+              magnam adipisci minima ducimus ipsam blanditiis.
+            </p>
+          </div>
+        </Link>
+      </article>
+    </div>
+  );
 };
 
 export default MostViewed;
