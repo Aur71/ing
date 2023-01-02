c9c4be19d8cb94f5a2541f2158111f40e02fe045
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-01-02 14:55:22 +0200

    most viewed done

--- components/home/MostViewed.jsx
+++ components/home/MostViewed.jsx
@@ -4,72 +4,65 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 // MEDIA
-import img from '../../public/temp/computer.jpg';
+import img1 from '../../public/temporary/img-1.jpg';
+import img2 from '../../public/temporary/img-2.jpg';
+import img3 from '../../public/temporary/img-3.jpg';
 
 const MostViewed = () => {
   return (
     <div className={styles.mostviewed}>
       <article className={styles.card}>
-        <div className={styles.imgContainer}>
-          <Image src={img} alt='thumbnail' />
-        </div>
-
         <Link href='#'>
+          <Image src={img1} alt='thumbnail' />
+
           <div className={styles.textContainer}>
             <h3>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
-              eligendi.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+              aliquid!
             </h3>
 
             <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
-              est officiis nulla, corporis sit veniam, iusto repudiandae impedit
-              accusantium totam obcaecati voluptate, accusamus consequatur
-              magnam adipisci minima ducimus ipsam blanditiis.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus
+              pariatur, corporis eos voluptatum laboriosam mollitia sapiente
+              nisi consequatur recusandae fuga.
             </p>
           </div>
         </Link>
       </article>
 
       <article className={styles.card}>
-        <div className={styles.imgContainer}>
-          <Image src={img} alt='thumbnail' />
-        </div>
-
         <Link href='#'>
+          <Image src={img2} alt='thumbnail' />
+
           <div className={styles.textContainer}>
             <h3>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
-              eligendi.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+              aliquid!
             </h3>
 
             <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
-              est officiis nulla, corporis sit veniam, iusto repudiandae impedit
-              accusantium totam obcaecati voluptate, accusamus consequatur
-              magnam adipisci minima ducimus ipsam blanditiis.
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus
+              pariatur, corporis eos voluptatum laboriosam mollitia sapiente
+              nisi consequatur recusandae fuga.
             </p>
           </div>
         </Link>
       </article>
 
       <article className={styles.card}>
-        <div className={styles.imgContainer}>
-          <Image src={img} alt='thumbnail' />
-        </div>
-
         <Link href='#'>
+          <Image src={img3} alt='thumbnail' />
+
           <div className={styles.textContainer}>
             <h3>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
-              eligendi.
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Reprehenderit possimus, temporibus rerum porro recusandae autem!
             </h3>
 
             <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
-              est officiis nulla, corporis sit veniam, iusto repudiandae impedit
-              accusantium totam obcaecati voluptate, accusamus consequatur
-              magnam adipisci minima ducimus ipsam blanditiis.
+              <span>Jhon Doe</span>
+              <span> · </span>
+              <span>December 29, 2022</span>
             </p>
           </div>
         </Link>
