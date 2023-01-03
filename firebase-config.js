2193451c11f4f8e1c4b7816edbbfc1fb72d68821
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-01-03 16:09:51 +0200

    test

--- firebase-config.js
+++ firebase-config.js
@@ -2,20 +2,23 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { initializeFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: `${process.env.API_KEY}`,
-  authDomain: `${process.env.AUTH_DOMAIN}`,
-  projectId: `${process.env.PROJECT_ID}`,
-  storageBucket: `${process.env.STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.MESSAGING_SENDER_ID}`,
-  appId: `${process.env.APP_ID}`,
+  apiKey: 'AIzaSyCc6Oqm0hH85sulTguOEM_GIZ62VFx7PC4',
+  authDomain: 'ing1-8edbf.firebaseapp.com',
+  projectId: 'ing1-8edbf',
+  storageBucket: 'ing1-8edbf.appspot.com',
+  messagingSenderId: '621353818689',
+  appId: '1:621353818689:web:d67895028e90c6825cc4c0',
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const storage = getStorage(app);
 
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
 
 export const auth = getAuth(app);
