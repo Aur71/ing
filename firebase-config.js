bf52af0bbee5e1e7cf1a0f1d5893c9381945a759
Aur71 <111057774+Aur71@users.noreply.github.com>
2023-01-03 19:04:08 +0200

    env added

--- firebase-config.js
+++ firebase-config.js
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 import { initializeFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCc6Oqm0hH85sulTguOEM_GIZ62VFx7PC4',
-  authDomain: 'ing1-8edbf.firebaseapp.com',
-  projectId: 'ing1-8edbf',
-  storageBucket: 'ing1-8edbf.appspot.com',
-  messagingSenderId: '621353818689',
-  appId: '1:621353818689:web:d67895028e90c6825cc4c0',
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
